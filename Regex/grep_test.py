e0149886fbf695ffb770da77c370ac31887133c2
Marchal Florent <florent.marchal@etu.umontpellier.fr>
2024-08-21 01:45:23 +0200

    added : Anotation in 'grep_test.py'

--- Regex/grep_test.py
+++ Regex/grep_test.py
@@ -1,53 +1,68 @@
 import re
 
+# Variables used inside the codeml wrapper
 method = 'BEB'
 method_full = "Bayes"
 nsites = 663 #221
 rst = open("./rst_copy")
 text  = rst.read()
-
 reg = []
 
-# ------------ Added recently to catch lnL ------------
+# ------------ catch lnL ------------
 reg.append('(?:^lnL = (.*))?$')
 reg.append('')
-# ------------ ----- -------- -- ----- --- ------------
+# ------------ --------- ------------
 
 # OLD :    '^{0} Empirical Bayes \({0[0]}EB\) probabilities for (\d+) classes( \(class\))?( & postmean_w)?( & P\(w>1\))?'.format(method_full)
 reg.append('^{0} Empirical Bayes \({0[0]}EB\) probabilities for (\d+) classes( \(class\))?( & postmean_w)?'.format(method_full))
 reg.append('\(amino acids refer to 1st sequence: seq-1\)')
 reg.append('')
 
-'''for i in range(nsites):
-    reg.append('\s+{0}\s+[A-Z-].+'.format(i+1))
-    if i == 0:
-        reg[-1] = "(" + reg[-1]
-reg[-1] += ")"'''
-researched_patern = r"\s+[0-9]+\s+[A-Z-].+"
+#  ------------ catch sites ------------
+# OLD :
+#    for i in range(nsites):
+#       reg.append('\s+{0}\s+[A-Z-].+'.format(i+1))
+#       if i == 0:
+#           reg[-1] = "(" + reg[-1]
+#     reg[-1] += ")
+
+researched_patern = r"\s+[0-9]+\s+[A-Z-].+" # Capture All sites in one command
 reg.append(rf'((?:{researched_patern})+)')
 
+#  ------------ ----------- ------------
 
 
-# -----
+
+#  ------------ Identify next title ------------
 reg.append('')
 reg.append('Positively selected sites')
 reg.append('')
 reg.append(r"\tProb\(w>1\).*mean w")
-
-#  r"^[1-{}][a-zA-Z]"
 reg.append('')
+#  ------------ ------------------- ------------
+
+
+#  ------------ Catch positively selected sites ------------
 researched_patern = r"\s+[0-9]+\s+[A-Z-].*"
 reg.append(rf'((?:{researched_patern})*)')
+#  ------------ ------------------------------- ------------
 
-# -----
-
+# Test regex
 reg = '\n'.join(reg)
 
-
-# mo = re.search(r'\n.*\nPositively selected sites', doct, re.MULTILINE)
 mo = re.search(reg, text , re.MULTILINE)
-print(mo.group())
-print("------------------")
-print(mo.group(1))
-# print(reg)
+# print(mo.group())
+# print("------------------")
+print(mo.group(6))
+
+
+# Groups :
+# 1 lnL
+# 2 number of classes
+# 3 "(class)"
+# 4 test type (& postmean_w)
+# 5 All sites
+# 6 All positively selected sites
+
+
 rst.close()
